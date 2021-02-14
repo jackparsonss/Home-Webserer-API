1530d7dafa290305a32fb1fd962e2ae31910c67a
Jack Parsons <jackpar1113@gmail.com>
2021-02-13 21:27:15 -0700

    convert data into json format

--- Data/get_data.py
+++ Data/get_data.py
@@ -1,18 +1,26 @@
 import serial
 from time import sleep
 from gpiozero import LED
+from datetime import datetime
+import json
 
 led = LED(2)
-
+data = {}
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=5)
 
 while True:
     try:
         sleep(1)
         temp = str(float(ser.readline().decode("utf-8").strip()))
-        print("Temp: " + temp + "C")
+        #print("Temp: " + temp + "C")
         humidity = str(float(ser.readline().decode("utf-8").strip()))
-        print("Humidity: " + humidity + "%")
+        #print("Humidity: " + humidity + "%")
+        data[datetime.now()] = {
+            "Temperature" : temp,
+            "Humidity" : humidity
+        }
+        data_set = json.dumps(data)
+        print(json.load(data_set))
     except:
         led.on()
         sleep(2)
