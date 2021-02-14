984a0f23a5051f70a8f9adef98409e1e0793fb1a
Jack Parsons <jackpar1113@gmail.com>
2021-02-13 21:19:48 -0700

    add humidity data

--- Data/get_data.py
+++ Data/get_data.py
@@ -9,8 +9,10 @@ ser = serial.Serial('/dev/ttyACM0', 9600, timeout=5)
 while True:
     try:
         sleep(1)
-        out = str(float(ser.readline().decode("utf-8").strip()))
-        print("Temp: " + out + "C")
+        temp = str(float(ser.readline().decode("utf-8").strip()))
+        print("Temp: " + temp + "C")
+        humidity = str(float(ser.readline().decode("utf-8").strip()))
+        print("Humidity: " + humidity + "%")
     except:
         led.on()
         sleep(2)
