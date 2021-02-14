20ac753e71b2c969192edab0fa8a37853f4ba331
Jack Parsons <jackpar1113@gmail.com>
2021-02-13 21:38:57 -0700

    fix json data

--- Data/get_data.py
+++ Data/get_data.py
@@ -5,23 +5,25 @@ from datetime import datetime
 import json
 
 led = LED(2)
-data = {}
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=5)
 
 while True:
     try:
+        data = {}
         led.off()
         sleep(1)
         temp = str(float(ser.readline().decode("utf-8").strip()))
         #print("Temp: " + temp + "C")
         humidity = str(float(ser.readline().decode("utf-8").strip()))
         #print("Humidity: " + humidity + "%")
-        data[datetime.now()] = {
+        time = datetime.now()
+        data[str(time)[:-7]] = {
             "Temperature" : temp,
             "Humidity" : humidity
         }
         data_set = json.dumps(data)
-        print(json.load(data_set))
-    except:
+        print(json.loads(data_set))
+    except Exception as ex:
+        print(ex)
         led.on()
         sleep(2)
