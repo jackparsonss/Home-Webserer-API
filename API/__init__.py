f3e4ec25ddbe95c5c56baff07b20218ec4bde61f
Jack Parsons <jackpar1113@gmail.com>
2021-02-15 13:55:45 -0700

    hello world welcome page

--- API/__init__.py
+++ API/__init__.py
@@ -5,7 +5,9 @@ def create_app():
     app = Flask(__name__)
     api = Api(app)
 
-    from .views import Sensor
+    from .views import Sensor, views
+
+    app.register_blueprint(views, url_prefix='/')
 
     api.add_resource(Sensor, '/api/get-data')
 
--- API/views.py
+++ API/views.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Blueprint
 from flask_restful import Resource
-from Data import get_data
+#from Data import get_data
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+def home():
+    return "<h1>Hello World</h1>"
+
 
 class Sensor(Resource):
     def get(self):
-        data = get_data
+        #data = get_data
+        data = {}
         return data
