55be1381d3ae361a8934e1c86f7716483b4a2fd0
Jack Parsons <jackpar1113@gmail.com>
2021-02-15 18:52:05 -0700

    test fetch request

--- API/views.py
+++ API/views.py
@@ -6,7 +6,7 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template("fetchData.html")
+    return render_template("index.html")
 
 
 class Sensor(Resource):
